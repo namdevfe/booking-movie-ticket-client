74ae5abae7f5d85c9eff11e92b23025461b62800
namnkq <namnkq.xherozone@gmail.com>
2025-07-21 13:00:57 +0700

    chore: add styles guide

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 const HomePage = () => {
   return (
-    <h1 className="text-4xl text-red-500 underline">Hello World</h1>
+    <main className='flex items-center justify-center h-screen'>
+      <button className='w-fit h-10 flex items-center justify-center px-2 py-1 bg-secondary transition-colors duration-300 hover:bg-[var(--hover-bg-btn-secondary-cl)]'>Button</button>
+    </main>
   )
 }
 
